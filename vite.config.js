0a938d21bc2295e8eb2a89997165734a23be4bcf
Aiden Schrock <aidennoel@proton.me>
2023-09-07 16:26:02 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import glsl from 'vite-plugin-glsl'
+import path from 'path';
 
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
@@ -20,5 +21,10 @@ export default {
     plugins:
         [
             glsl()
-        ]
+        ],
+    resolve: {
+        alias: {
+            'shader-park-core': path.resolve(__dirname, 'node_modules/shader-park-core/dist/shader-park-core.esm.js')
+        }
+    }
 }
